7a0546fb0c2d1b0e4aafc2961ebd26f61fe7ce51
Jamieson <158116310+ash-comsci@users.noreply.github.com>
2024-10-14 09:07:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 /**
- * ICS3 Template
-**/
+ * 2.2 - Variables and Math
+ * ICS3 - Mr. J
+ * 
+ * Make sure to read the README.md (especially if you missed class)
+ * Then checkout the YOUR_TASK.md to see the practice plan
+ * 
+ * Don't forget - these are playgrounds. Once you finish the requested task, feel free to play.
+ **/ 
+
+
+// Some example variables
+let it_is_christmas = false
+let teaching_partner = "Mr. Sutcliffe"
+let number_of_students = 24
